75a6c422e7ad76eca7271f4d5c45aeaab8392346
Fagbenle Emmanuel <yinkafagbenle@gmail.com>
2026-06-11 11:32:22 +0100

    Clean Arch - Create Signup Page

---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Name',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
