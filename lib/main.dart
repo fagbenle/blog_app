b7c0ca85711aa09b47de0ed61e3d0b187775c69d
Fagbenle Emmanuel <yinkafagbenle@gmail.com>
2026-06-11 11:36:17 +0100

    Renamed the app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Blog App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
